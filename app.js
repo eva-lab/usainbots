fc16207bee148ec2e160115c29987a832d699bd4
Jorge Linhares <jorgehclinhares@gmail.com>
2016-10-26 20:54:35 -0300

    Refactor: Aceitando cabeçalho Authorization

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*'); //TODO: Ajustar para aceitar apenas as aplicações com permissão
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
